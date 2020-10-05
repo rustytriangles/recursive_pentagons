dc2febe387bb0afe4f7266772de7e2c5de0d2599
Michael P Garrity <mike@mikegslptop2495.lan>
2020-10-05 13:39:31 -0400

    Updates

---
+++ precache-manifest.28be519f8a1b8cd2b6b6ca41aa772623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "782dc734ca976906ed9e839f98f57ce2",
+    "url": "/recursive_pentagons/index.html"
+  },
+  {
+    "revision": "62a3a4a845b2c7af2523",
+    "url": "/recursive_pentagons/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d16179a9772dd05d9e15",
+    "url": "/recursive_pentagons/static/js/2.dde30beb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recursive_pentagons/static/js/2.dde30beb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62a3a4a845b2c7af2523",
+    "url": "/recursive_pentagons/static/js/main.7b78155c.chunk.js"
+  },
+  {
+    "revision": "f3c27d337d2b38d1ff18",
+    "url": "/recursive_pentagons/static/js/runtime-main.bfb20891.js"
+  }
+]);
